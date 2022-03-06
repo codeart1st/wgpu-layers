c18206c0fdaa8819486789979a49b31f234c0344
Paul Lange <paul.lange@data-experts.de>
2022-03-06 11:25:31 +0100

    Added console.log support in browser

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,27 @@
-mod utils;
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+  use wasm_bindgen::prelude::*;
+  use log::info;
 
-use wasm_bindgen::prelude::*;
+  // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+  // allocator.
+  #[cfg(feature = "wee_alloc")]
+  #[global_allocator]
+  static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
+  #[wasm_bindgen]
+  extern {
     fn alert(s: &str);
-}
+  }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wgpu-layers2!");
+  #[wasm_bindgen]
+  pub fn greet() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+
+    #[cfg(feature = "console_log")]
+    console_log::init_with_level(log::Level::Info).expect("error initializing logger");
+
+    info!("Hello, wgpu-layers!");
+  }
 }
