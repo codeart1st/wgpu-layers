9909d00cd0f0c0c8b330a9f107365244ec572291
Paul Lange <paul.lange@data-experts.de>
2024-05-25 16:20:30 +0200

    fix: winit api changes / deprecations

--- examples/window.rs
+++ examples/window.rs
@@ -1,99 +1,102 @@
 #[cfg(not(target_arch = "wasm32"))]
 mod example {
   use log::info;
+  use pollster::FutureExt;
   use std::sync::Arc;
 
-  struct MapWindow {
-    window: Arc<winit::window::Window>,
+  struct Application {
+    window: Option<Arc<winit::window::Window>>,
+    size: winit::dpi::PhysicalSize<u32>,
   }
 
-  impl wgpu_layers::renderer::ToSurface for MapWindow {
-    fn create_surface(
-      &self,
-      instance: &wgpu::Instance,
-    ) -> Result<wgpu::Surface<'static>, wgpu::CreateSurfaceError> {
-      instance.create_surface(wgpu::SurfaceTarget::Window(Box::new(self.window.clone())))
+  impl Application {
+    fn new() -> Self {
+      Self {
+        window: None,
+        size: winit::dpi::PhysicalSize::new(512, 512),
+      }
+    }
+    fn create_window(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+      let window_attributes = winit::window::Window::default_attributes()
+        .with_title("wgpu-map")
+        //.with_transparent(true)
+        .with_resizable(false)
+        .with_min_inner_size(self.size)
+        .with_max_inner_size(self.size)
+        .with_inner_size(self.size);
+
+      let window = event_loop
+        .create_window(window_attributes)
+        .expect("Window can't be created");
+
+      self.window = Some(Arc::new(window));
     }
   }
 
-  fn create_map_window() -> (winit::event_loop::EventLoop<()>, MapWindow) {
-    let event_loop = winit::event_loop::EventLoop::new().unwrap();
-    let size = winit::dpi::PhysicalSize::new(512, 512);
-    let window_builder = winit::window::WindowBuilder::new()
-      .with_title("wgpu-map")
-      //.with_transparent(true)
-      .with_resizable(false)
-      .with_min_inner_size(size)
-      .with_max_inner_size(size)
-      .with_inner_size(size);
+  impl winit::application::ApplicationHandler for Application {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+      info!("Resumed");
 
-    let window = window_builder
-      .build(&event_loop)
-      .expect("Window can't be created");
+      self.create_window(event_loop);
 
-    (
-      event_loop,
-      MapWindow {
-        window: Arc::new(window),
-      },
-    )
-  }
+      wgpu_layers::init(self, (self.size.width, self.size.height)).block_on();
+    }
 
-  async fn start(
-    event_loop: winit::event_loop::EventLoop<()>,
-    window: MapWindow,
-    size: winit::dpi::PhysicalSize<u32>,
-  ) {
-    wgpu_layers::init(&window, (size.width, size.height)).await;
-
-    #[rustfmt::skip]
-    let view_matrix = vec![
-      1.15142285e-7, -0.0, 0.0, 0.0,
-      0.0, 1.15142285e-7, 0.0, 0.0,
-      -0.27666306, -0.7963807, 1.0, 0.0,
-      0.0, 0.0, 0.0, 1.0,
-    ];
-    wgpu_layers::render(view_matrix, vec![size.width, size.height]);
-
-    info!("renderer init");
-
-    let _ = event_loop.run(move |event, elwt| {
-      if let winit::event::Event::WindowEvent { event, .. } = event {
-        match event {
-          winit::event::WindowEvent::CloseRequested => elwt.exit(),
-          winit::event::WindowEvent::KeyboardInput {
-            event:
-              winit::event::KeyEvent {
-                state: winit::event::ElementState::Pressed,
-                logical_key,
-                ..
-              },
-            ..
-          } => match logical_key {
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::Enter) => elwt.exit(),
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowUp) => {}
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowDown) => {}
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowLeft) => {}
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowRight) => {}
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::PageUp) => {}
-            winit::keyboard::Key::Named(winit::keyboard::NamedKey::PageDown) => {}
-            _ => (),
-          },
-          winit::event::WindowEvent::Resized(_) => {}
-          winit::event::WindowEvent::RedrawRequested => {
-            #[rustfmt::skip]
+    fn window_event(
+      &mut self,
+      event_loop: &winit::event_loop::ActiveEventLoop,
+      _window_id: winit::window::WindowId,
+      event: winit::event::WindowEvent,
+    ) {
+      match event {
+        winit::event::WindowEvent::CloseRequested => event_loop.exit(),
+        winit::event::WindowEvent::KeyboardInput {
+          event:
+            winit::event::KeyEvent {
+              state: winit::event::ElementState::Pressed,
+              logical_key,
+              ..
+            },
+          ..
+        } => match logical_key {
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::Enter) => event_loop.exit(),
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowUp) => {}
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowDown) => {}
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowLeft) => {}
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::ArrowRight) => {}
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::PageUp) => {}
+          winit::keyboard::Key::Named(winit::keyboard::NamedKey::PageDown) => {}
+          _ => (),
+        },
+        winit::event::WindowEvent::Resized(size) => {
+          self.size = size;
+        }
+        winit::event::WindowEvent::RedrawRequested => {
+          #[rustfmt::skip]
             let view_matrix = vec![
               1.15142285e-7, -0.0, 0.0, 0.0,
               0.0, 1.15142285e-7, 0.0, 0.0,
               -0.27666306, -0.7963807, 1.0, 0.0,
               0.0, 0.0, 0.0, 1.0,
             ];
-            wgpu_layers::render(view_matrix, vec![size.width, size.height]);
-          }
-          _ => (),
+          wgpu_layers::render(view_matrix, vec![self.size.width, self.size.height]);
         }
+        _ => (),
       }
-    });
+    }
+  }
+
+  impl wgpu_layers::renderer::ToSurface for Application {
+    fn create_surface(
+      &self,
+      instance: &wgpu::Instance,
+    ) -> Result<wgpu::Surface<'static>, wgpu::CreateSurfaceError> {
+      instance.create_surface(wgpu::SurfaceTarget::Window(Box::new(
+        <std::option::Option<std::sync::Arc<winit::window::Window>> as Clone>::clone(&self.window)
+          .unwrap()
+          .clone(),
+      )))
+    }
   }
 
   pub fn main() {
@@ -101,10 +104,10 @@ mod example {
       env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
 
-    let (event_loop, map_window) = create_map_window();
-    let size = map_window.window.inner_size();
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let mut application = Application::new();
 
-    pollster::block_on(start(event_loop, map_window, size));
+    let _ = event_loop.run_app(&mut application);
   }
 }
 
